7dbd059d05274796611a06579c50e97305485751
Juud1 <micajudith637@gmail.com>
2023-10-07 17:29:59 -0300

    Commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import Bootloader from "../src/Bootloader.js    ";
+import Scene_play from "../src/escenas/Scene_play.js";
+
+const config = {
+    title: "FPWTpExpress",
+    version: "0.0.1",
+    type: Phaser.AUTO,
+    scale: {
+        parent: "phaser_container",
+        width: 800,
+        height: 600,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    backgroundColor: "#ffffff",
+    pixelArt: false,
+    physics: {
+        default: "arcade",
+        "arcade": {
+            gravity: {
+                y: 0
+            }
+        }
+    },
+    scene: [
+        Bootloader,
+        Scene_play
+    ]
+};
+
+new Phaser.Game(config);
